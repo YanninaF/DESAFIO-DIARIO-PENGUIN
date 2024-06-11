0851d87e47ed9824572df5948d4957368ca919fa
Yannina Fariña <91395029+YanninaF@users.noreply.github.com>
2024-06-11 14:31:55 -0400

    DIA 1
    11/06/2024 
    
    Inversión de una Cadena: Escribe un programa que invierta una cadena de caracteres dada por el usuario.

---
+++ dia1.py
@@ -0,0 +1,10 @@
+def invertir_caracteres(cadena_de_caracteres):
+    if len(cadena_de_caracteres) == 0:
+        return ""
+    else:
+        return cadena_de_caracteres[-1] + invertir_caracteres(cadena_de_caracteres[:-1])
+
+resultado = invertir_caracteres('Yannina')
+print(resultado)
+
+
